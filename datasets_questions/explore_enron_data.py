06c7eca7eece364258a53825b95d23caf0b59e5f
Marco <marco.ceccarelli@spot.it>
2017-10-07 17:06:17 +0200

    enron analisys

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -20,3 +20,26 @@ import pickle
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
 
+print "number of data points: ", len(enron_data)
+
+print "number of features per person: ", len(enron_data["SKILLING JEFFREY K"])
+
+print "number of poi: ", sum( x["poi"] == 1 for x in enron_data.values() )
+
+print enron_data["PRENTICE JAMES"]["total_stock_value"]
+
+print enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+
+print enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+
+print enron_data["SKILLING JEFFREY K"]["total_payments"]
+print enron_data["LAY KENNETH L"]["total_payments"]
+print enron_data["FASTOW ANDREW S"]["total_payments"]
+
+print "number of point with salary: ", sum( x["salary"] != "NaN" for x in enron_data.values() )
+
+print "number of point with email_address: ", sum( x["email_address"] != "NaN" for x in enron_data.values() )
+
+print "number of point without total_payments: ", sum( x["total_payments"] == "NaN" for x in enron_data.values() )
+
+print "number of poi without total_payments: ", sum( x["total_payments"] == "NaN" and x["poi"] == 1 for x in enron_data.values() )
