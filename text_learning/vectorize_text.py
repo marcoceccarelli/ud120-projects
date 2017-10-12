ba107c58dddb995c324d8714f1fbbc2269a05e16
Marco <marco.ceccarelli@spot.it>
2017-10-12 22:45:20 +0200

    text learning

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -23,53 +23,74 @@ from parse_out_email_text import parseOutText
 """
 
 
-from_sara  = open("from_sara.txt", "r")
-from_chris = open("from_chris.txt", "r")
+# from_sara  = open("from_sara.txt", "r")
+# from_chris = open("from_chris.txt", "r")
 
-from_data = []
-word_data = []
+# from_data = []
+# word_data = []
 
-### temp_counter is a way to speed up the development--there are
-### thousands of emails from Sara and Chris, so running over all of them
-### can take a long time
-### temp_counter helps you only look at the first 200 emails in the list so you
-### can iterate your modifications quicker
-temp_counter = 0
-
-
-for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
-    for path in from_person:
-        ### only look at first 200 emails when developing
-        ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
-            print path
-            email = open(path, "r")
-
-            ### use parseOutText to extract the text from the opened email
-
-            ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
-
-            ### append the text to word_data
-
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+# ### temp_counter is a way to speed up the development--there are
+# ### thousands of emails from Sara and Chris, so running over all of them
+# ### can take a long time
+# ### temp_counter helps you only look at the first 200 emails in the list so you
+# ### can iterate your modifications quicker
+# temp_counter = 0
 
 
-            email.close()
+# for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
+#     for path in from_person:
+#         ### only look at first 200 emails when developing
+#         ### once everything is working, remove this line to run over full dataset
+#         #temp_counter += 1
+#         if temp_counter < 200:
+#             path = os.path.join('..', path[:-2])
+#             print path
+#             email = open(path, "r")
 
-print "emails processed"
-from_sara.close()
-from_chris.close()
+#             ### use parseOutText to extract the text from the opened email
+#             text = parseOutText(email)
 
-pickle.dump( word_data, open("your_word_data.pkl", "w") )
-pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+#             ### use str.replace() to remove any instances of the words
+#             ### ["sara", "shackleton", "chris", "germani"]
+#             text = text.replace("sara", "")
+#             text = text.replace("shackleton", "")
+#             text = text.replace("chris", "")
+#             text = text.replace("germani", "")
 
+#             ### append the text to word_data
+#             word_data.append(text)
 
+#             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+#             if(name == "sara"):
+#                 from_data.append(0)
+#             else:
+#                 from_data.append(1)
 
+#             email.close()
+
+# print "emails processed"
+# from_sara.close()
+# from_chris.close()
+
+# pickle.dump( word_data, open("your_word_data.pkl", "w") )
+# pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+
+# print "word_data[152] ", word_data[152]
 
 
 ### in Part 4, do TfIdf vectorization here
 
+word_data = pickle.load(open("your_word_data.pkl", "r"))
 
+from sklearn.feature_extraction import text
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vectorizer = TfidfVectorizer(stop_words=text.ENGLISH_STOP_WORDS)
+
+X = vectorizer.fit_transform(word_data)
+
+#idf_values = dict(zip(vectorizer.get_feature_names(), vectorizer.idf_))
+
+print "len words", len(vectorizer.get_feature_names())
+
+print "word 34597", vectorizer.get_feature_names()[34597]
