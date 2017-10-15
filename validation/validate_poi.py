3403464ed779ed6c583ac8e85fd6b4894279b01d
Marco <marco.ceccarelli@spot.it>
2017-10-15 16:36:05 +0200

    validation

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -15,7 +15,7 @@ import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
@@ -28,5 +28,20 @@ labels, features = targetFeatureSplit(data)
 
 
 ### it's all yours from here forward!  
+from sklearn import cross_validation
+
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
+
+from sklearn import tree
+
+from sklearn.metrics import accuracy_score
 
 
+clf = tree.DecisionTreeClassifier()
+
+clf = clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+accuracy = accuracy_score(pred, labels_test)
+print "accuracy:", accuracy
